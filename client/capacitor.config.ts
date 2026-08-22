915a1f64e64ac9b18ace2e538630f1256955daf2
jubnl <jgunther021@gmail.com>
2026-08-22 22:52:25 +0200

    chore(capacitor): base files for capacitor

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.liketrek.trek',
+  appName: 'TREK - Plan your trips!',
+  webDir: 'dist'
+};
+
+export default config;
